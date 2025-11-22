b0f4aca2ee3ddbba7a3974e12649564e9888374d
kuldeepparmar <kuldeep811p@gmail.com>
2025-11-22 20:29:34 +0530

    Create project_vit.py

---
+++ project_vit.py
@@ -0,0 +1,17 @@
+print("Simple Calculator")
+print("1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+
+choice = int(input("Enter choice (1-4): "))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Result:", a + b)
+elif choice == 2:
+    print("Result:", a - b)
+elif choice == 3:
+    print("Result:", a * b)
+elif choice == 4:
+    print("Result:", a / b)
+else:
+    print("Invalid Choice")
